5b29e1117cd9e2f3aa0d11bdc53e24e38aeab65e
yzj123 <3413553817@qq.com>
2024-10-06 09:55:29 +0800

    modified

--- Solution18.java
+++ Solution18.java
@@ -1,49 +1,49 @@
 import java.util.*;
 
 /*
- * @Description:
- * 
- * 除自身以外数组的乘积
- * 给你一个整数数组 nums，返回数组 answer，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
- * 题目数据保证数组 nums 之中任意元素的全部前缀元素和后缀的乘积都在 32 位 整数范围内。
- * 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
- * 
- * 示例 1:
- * 输入: nums = [1,2,3,4]
- * 输出: [24,12,8,6]
- *  示例 2:
- * 输入: nums = [-1,1,0,-3,3]
- * 输出: [0,0,9,0,0]
+ * @Description
+ * 重复 DNA 序列
+ * DNA 序列 由一系列核苷酸组成，缩写为 'A', 'C', 'G' 和 'T'.。
+ * 例如，"ACGAATTCCG" 是一个 DNA 序列 。
+ * 在研究 DNA 时，识别 DNA 中的重复序列非常有用。
+ * 给定一个表示 DNA 序列 的字符串 s ，返回所有在DNA分子中出现不止一次的长度为10的序列(子字符串)。你可以按任意顺序返回答案。
+ *
+ *
+ * 示例 1：
+ * 输入：s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+ * 输出：["AAAAACCCCC","CCCCCAAAAA"]
+ * 示例 2：
+ * 输入：s = "AAAAAAAAAAAAA"
+ * 输出：["AAAAAAAAAA"]
  */
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int length === nums.length;
+    static final int L = 10;
+    public static Object findRepeatedDnaSequences;
+    static Map<Character, Integer> bin = new HashMap<Character, Integer>() {{
+        put('A', 0);
+        put('C', 1);
+        put('G', 2);
+        put('T', 3);
+    }};
 
-        // L 和 R 分别表示左右两侧的乘积列表
-        int[] L = new int[length];
-        int[] R = new int[length];
-
-        int[] answer = new int[length];
-
-        // L[i] 为索引 i 左侧所有元素的乘积
-        // 对于索引为 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
-        L[0] = 1;
-        for (int i = 1; i < length; i++) {
-            L(i) = nums[i - 1] * L[i - 1];
+    public static List<String> findRepeatedDnaSequences(String s) {
+        List<String> ans = new ArrayList<String>();
+        int n = s.length();
+        if (n <= L) {
+            return ans;
         }
-
-        // R[i] 为索引 i 右侧所有元素的乘积
-        // 对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
-        R[length - 1] = 1;
-        for (int i = length - 2; i >= 0; i--) {
-            R[i] = nums[i + 1] * R[i + 1];
+        int x = 0;
+        for (int i = 0; i < L - 1; ++i) {
+            x = (x << 2) | bin.get(s.charAt(i));
         }
-
-        // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
-        for {int i = 0; i < length; i++} {
-            answer[i] = L[i] * R[i];
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        for (int i = 0; i <= n - L; ++i) {
+            x = ((x << 2) | bin.get(s.charAt(i + L - 1))) & ((1 << (L * 2)) - 1);
+            cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+            if (cnt.get(x) == 2) {
+                ans.add(s.substring(i, i + L));
+            }
         }
-
-        return answer;
+        return ans;
     }
 }
