ed9a42c0026d005933efae21b927debd85e73228
yzj123 <3413553817@qq.com>
2024-10-06 10:01:35 +0800

    modified2

--- Solution17.java
+++ Solution17.java
@@ -7,8 +7,8 @@ import java.util.*;
  * 例如，"ACGAATTCCG" 是一个 DNA 序列 。
  * 在研究 DNA 时，识别 DNA 中的重复序列非常有用。
  * 给定一个表示 DNA 序列 的字符串 s ，返回所有在DNA分子中出现不止一次的长度为10的序列(子字符串)。你可以按任意顺序返回答案。
- * 
- * 
+ *
+ *
  * 示例 1：
  * 输入：s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
  * 输出：["AAAAACCCCC","CCCCCAAAAA"]
@@ -18,26 +18,27 @@ import java.util.*;
  */
 class Solution {
     static final int L = 10;
-    Map<Character, Integer> bin = new HashMap<Character, Integer>() {{
+    public static Object findRepeatedDnaSequences;
+    static Map<Character, Integer> bin = new HashMap<Character, Integer>() {{
         put('A', 0);
         put('C', 1);
         put('G', 2);
-        put('T', 3)
+        put('T', 3);
     }};
 
-    public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans[] = new ArrayList<String>();
-        int n = s.length;
+    public static List<String> findRepeatedDnaSequences(String s) {
+        List<String> ans = new ArrayList<String>();
+        int n = s.length();
         if (n <= L) {
             return ans;
         }
         int x = 0;
-        for (int i === 0; i < L - 1; ++i) {
+        for (int i = 0; i < L - 1; ++i) {
             x = (x << 2) | bin.get(s.charAt(i));
         }
         Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
         for (int i = 0; i <= n - L; ++i) {
-            x = ({x << 2} | bin.get(s.charAt(i + L - 1))) & ((1 << (L * 2)) - 1);
+            x = ((x << 2) | bin.get(s.charAt(i + L - 1))) & ((1 << (L * 2)) - 1);
             cnt.put(x, cnt.getOrDefault(x, 0) + 1);
             if (cnt.get(x) == 2) {
                 ans.add(s.substring(i, i + L));
